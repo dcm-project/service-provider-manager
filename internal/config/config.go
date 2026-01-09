c6d598a71b659bb8b601d1ab0282575b0be71216
Gloria Ciavarrini <gciavarrini@redhat.com>
2026-01-09 11:51:18 +0100

    Add config package with environment-based loading
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Database *DBConfig
+	Service  *ServiceConfig
+}
+
+type DBConfig struct {
+	Type     string `envconfig:"DB_TYPE" default:"pgsql"`
+	Hostname string `envconfig:"DB_HOST" default:"localhost"`
+	Port     string `envconfig:"DB_PORT" default:"5432"`
+	Name     string `envconfig:"DB_NAME" default:"service-provider"`
+	User     string `envconfig:"DB_USER" default:"admin"`
+	Password string `envconfig:"DB_PASS" default:"adminpass"`
+}
+
+type ServiceConfig struct {
+	Address  string `envconfig:"SVC_ADDRESS" default:":8080"`
+	LogLevel string `envconfig:"SVC_LOG_LEVEL" default:"info"`
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{}
+	if err := envconfig.Process("", cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
